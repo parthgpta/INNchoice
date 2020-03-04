5b946dfdc8df6a9150683b08920360cfb28e98f9
Parth Gupta <51705666+parthgpta@users.noreply.github.com>
2020-03-04 15:50:32 +0530

    Update models.py

--- innchoice/bookings/models.py
+++ innchoice/bookings/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+# import datetime
 from accounts.models import person
 from django.contrib.auth.models import User
 
@@ -6,17 +7,34 @@ class Constants:
     room_type =(
         ('Suite' , 'Suite'),
         ('Deluxe' , 'Deluxe'),
-        ('Standard' , 'Standard')
     )
 
 
-class Room(models.Model):
-    manager = models.ForeignKey(person , related_name='manager', on_delete=models.CASCADE)
-    customer = models.ForeignKey(person ,related_name='customer' ,  on_delete=models.CASCADE)
-    checkin = models.DateField(auto_now_add=True)
-    room_type =models.CharField(choices=Constants.room_type , max_length=8 , default='Suite')
-    price  = models.IntegerField()
-    checkout = models.DateField(auto_now_add=True)
+class timeslots(models.Model):
+    starttime = models.IntegerField()
+    endtime = models.IntegerField()
 
     def __str__(self):
-        return "{0}-{1}".format(self.room_type , self.manager)
+        return "{}-{}".format(self.starttime, self.endtime)
+
+
+class available(models.Model):
+    manager = models.ForeignKey(person , on_delete=models.CASCADE)
+    number = models.IntegerField(default=30)
+    day = models.IntegerField()
+    month = models.IntegerField()
+    year = models.IntegerField()
+    room_type = models.CharField(max_length= 10 , default='Suite' , choices= Constants.room_type)
+    starttime = models.IntegerField()
+    endtime = models.IntegerField()
+
+    def __str__(self):
+        return "{}".format(self.manager)
+
+class number(models.Model):
+    number = models.IntegerField(default=30)
+    day_adv = models.IntegerField(default=5)
+
+    def __str__(self):
+        return '{}-{}'.format(self.number,self.day_adv)
+
